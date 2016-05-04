c72157049d2fe7e68913775fd59fba28276279ac
ATHUL S NAMBIAR <athulsnambiarap@yahoo.com>
2016-05-04 11:26:32 +0530

    incomplete function testing stage

---
+++ train.cpp
@@ -0,0 +1,162 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "/home/qburst/opencv3/dlib-18.18/dlib/image_processing/frontal_face_detector.h"
+#include "/home/qburst/opencv3/dlib-18.18/dlib/image_processing/render_face_detections.h"
+#include "/home/qburst/opencv3/dlib-18.18/dlib/image_processing.h"
+#include "/home/qburst/opencv3/dlib-18.18/dlib/gui_widgets.h"
+#include "/home/qburst/opencv3/dlib-18.18/dlib/image_io.h"
+
+#include<iostream>
+#include<stdio.h>
+#include<sstream>
+#include<fstream>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+using namespace dlib;
+using namespace cv;
+
+
+typedef matrix<double,4556,1> sample_type;
+
+vector<vector <float> > getAttributesCSV(char * name);
+bool rowsAndCols(char *name,int &row,int &col);
+vector <int> getLabelsCSV(char * name);
+
+
+
+bool rowsAndCols(char *name,int &row,int &col)
+{
+	ifstream file(name);
+	
+	if (!file)
+	{
+		cout << "can not open file" << endl;
+		row = col = -1;
+		return false;
+	}
+	else
+	{
+		row = col = 0;
+		char c;
+		while (c = file.get())
+		{
+			if(!file.good())
+				break;
+			if(c == '\n')
+				row++,col++;
+			else if(c == ',')
+				col++;
+		}
+		
+		col = col/row;
+		file.close();
+		return true;
+	}
+}
+
+
+
+
+
+
+vector <int> getLabelsCSV(char * name)
+{
+
+	std::ifstream  file(name);
+	int cellValue;
+	int row,col,r,c;
+	rowsAndCols(name,row,col);
+	std::string line;
+	vector<int> labels;
+	
+	for(r = 0; r < row; r++)
+	{
+		getline(file,line);
+		stringstream lineStream(line);
+		string cell;
+		if(r == 0)
+			continue;
+		for(c = 0; c < col; c++)
+		{
+			
+			std::getline(lineStream,cell,',');
+			if(c != col-1)
+				continue;
+			stringstream cell1(cell);
+			cell1 >> cellValue;
+			labels.push_back(cellValue);
+		}
+	}
+	file.close();
+	return labels;
+}
+
+
+vector<vector <float> > getAttributesCSV(char * name)
+{
+
+	std::ifstream  file(name);
+	float cellValue;
+	int row,col,r,c;
+	rowsAndCols(name,row,col);
+	std::string line;
+	vector<vector <float> > Matrix;
+	
+	for(r = 0; r < row; r++)
+	{
+		getline(file,line);
+		stringstream lineStream(line);
+		vector <float> row1;
+		string cell;
+		if(r == 0)
+			continue;
+		for(c = 0; c < col; c++)
+		{
+			if(c == col-1)
+				continue;
+			std::getline(lineStream,cell,',');
+			stringstream cell1(cell);
+			cell1 >> cellValue;
+			row1.push_back(cellValue);
+		}
+		Matrix.push_back(row1);
+	}
+	file.close();
+	return Matrix;
+}
+
+
+void generateData(std::vector<sample_type>& samples,std::vector<double>& labels)
+{
+	vector<vector <float> > matrix = getAttributesCSV("points.csv");
+	vector <int> matLabel = getLabelsCSV("points.csv");
+	sample_type temp;
+	
+	
+	for(int i = 0; i < matrix.size();i++)
+	{
+		for(int j = 0; j < matrix[0].size();j++)
+		{
+			temp(j) = matrix[i][j];
+		}
+		samples.push_back(temp);
+		labels.push_back(matLabel[i]);
+	}
+}
+
+int main()
+{
+	try
+	{
+		std::vector<sample_type> samples;
+        std::vector<double> labels;
+        
+        generate_data(samples, labels);
+
+        cout << "samples.size(): "<< samples.size() << endl;
+	}
+}
+
