790c17034d77160abb5ab4aa14f23ebfe8f6c09b
ATHUL S NAMBIAR <athulsnambiarap@yahoo.com>
2016-05-10 10:39:39 +0530

    empty module for object detection

---
+++ objectDetection.cpp
@@ -0,0 +1,10 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
