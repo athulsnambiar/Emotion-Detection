86022e5466c96a5ca27aa277fba91eaea43f24b3
ATHUL S NAMBIAR <athulsnambiarap@yahoo.com>
2016-05-10 11:04:19 +0530

    simple image read and display

--- objectDetection.cpp
+++ objectDetection.cpp
@@ -8,3 +8,16 @@
 using namespace std;
 using namespace cv;
 
+int main(int argc,char **argv)
+{
+	float mask[] = {-1,0,1};
+	Mat image = imread(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
+	
+	namedWindow( "Display window", WINDOW_AUTOSIZE );
+    imshow( "Display window", image );
+	
+	waitKey(0);
+	destroyWindow("Display window");
+    return 0;
+	
+}
