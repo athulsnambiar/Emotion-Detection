3ffd096d61b27bb3c3efbfb472ced4ec55c7ea1b
ATHUL S NAMBIAR <athulsnambiarap@yahoo.com>
2016-05-17 14:56:20 +0530

    cleared a bug related to faceRectangle vector

--- annotate.cpp
+++ annotate.cpp
@@ -30,15 +30,15 @@ void removePhotos();
 int detectFaceAndCrop(char *imageName)
 {
 	frontal_face_detector detector = get_frontal_face_detector();
-	
+
 	array2d<rgb_pixel> img;
-	
+
 	load_image(img,imageName);
-	
+
 	pyramid_up(img);
-	
+
 	std::vector<dlib::rectangle> faceRectangles = detector(img);
-	
+
 	std::vector<full_object_detection> facialFeatures;
 
 	for (int j = 0; j < faceRectangles.size(); ++j)
@@ -46,32 +46,32 @@ int detectFaceAndCrop(char *imageName)
 		full_object_detection feature = sp(img, faceRectangles[j]);
 		facialFeatures.push_back(feature);
 	}
-	
+
 	dlib::array< array2d<rgb_pixel> > faces;
-	
+
 	extract_image_chips(img, get_face_chip_details(facialFeatures,500), faces);
-	
+
 	for(int i = 0; i < faces.size();i++,faceNumber++)
 	{
 		stringstream s;
 		s<<"face"<<(faceNumber)<<".jpg";
 		save_jpeg(faces[i],s.str(),100);
 	}
-	
-	return(faceRectangles.size());	
+
+	return(faceRectangles.size());
 }
 
 int storeAttributesToCSV(int noOfFaces,int emotion)
 {
 	int i,j,k;
 	frontal_face_detector detector = get_frontal_face_detector();
-	
+
 	ofstream outfile;
 	ifstream infile("points.csv");
 	stringstream s;
-	
+
 	outfile.open("points.csv",ios::app);
-	
+
 	if(!infile.good())
 	{
 		for(i = 0; i < 68;i++)
@@ -80,7 +80,7 @@ int storeAttributesToCSV(int noOfFaces,int emotion)
 					outfile<<"lena"<<i<<"b"<<j<<","<<"a"<<i<<"b"<<j<<",";
 		outfile<<"emotion\n";
 	}
-	
+
 	for(i = 0; i < faceNumber; i++)
 	{
 		array2d<rgb_pixel> img;
@@ -88,17 +88,20 @@ int storeAttributesToCSV(int noOfFaces,int emotion)
 		s<<"face"<<(i)<<".jpg";
 		load_image(img,s.str());
 		cout<<"writing image"<<(i-1)<<" to csv\n";
-		
+
 		std::vector<dlib::rectangle> faceRectangles = detector(img);
-	
+		if(faceRectangles.size() <= 0)
+		{
+			continue;
+		}
 		std::vector<full_object_detection> facialFeatures;
-		
+
 		full_object_detection feature = sp(img, faceRectangles[0]);
-		
+
 		for(int j = 0; j < 68; j++)
 			for(int k = 0; k < j; k++)
 				outfile<<length(feature.part(j),feature.part(k))<<","<<slope(feature.part(j),feature.part(k))<<",";
-		
+
 		outfile<<emotion<<"\n";
 	}
 	outfile.close();
@@ -110,14 +113,14 @@ int storeAttributesToCSV(int noOfFaces,int emotion)
 double length(point a,point b)
 {
 	int x1,y1,x2,y2;
-	double dist;	
+	double dist;
 	x1 = a.x();
 	y1 = a.y();
 	x2 = b.x();
 	y2 = b.y();
 
 	dist = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
-	dist = sqrt(dist);	
+	dist = sqrt(dist);
 	return dist;
 }
 
@@ -143,7 +146,7 @@ void removePhotos()
 {
 	int i;
 	stringstream s;
-	
+
 	for(i = 0; i < faceNumber; i++)
 	{
 		s.str("");
@@ -172,4 +175,3 @@ int main(int argc,char **argv)
 	removePhotos();
 	return 0;
 }
-
