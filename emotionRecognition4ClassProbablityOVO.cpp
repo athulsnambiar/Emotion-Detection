8271365f95095881fce5baee6e1c8eb17d746958
ATHUL S NAMBIAR <athulsnambiarap@yahoo.com>
2016-05-18 19:26:02 +0530

    removed a glich in loop

--- emotionRecognition4ClassProbablityOVO.cpp
+++ emotionRecognition4ClassProbablityOVO.cpp
@@ -184,21 +184,21 @@ std::vector<double> probablityCalculator(std::vector<double> P)
 {
   std::vector<double> EmoProb(4);
   float e[4],temp;
-  
-  for(int i=0;i<=11;i++)
+
+  for(int i=0;i < 6;i++)
   {
-     
+
      P.push_back(1-P[i]);
   }
-  
+
   e[0] = P[0]+P[1]+P[2];
   e[1] = P[3]+P[4]+P[6];
   e[2] = P[5]+P[7]+P[9];
   e[3] = P[8]+P[10]+P[11];
-  
+
   int  i, j;
   int t[]={0,1,2,3};
-  
+
   for(i=0;i<4;i++)
   {
     for(j=i+1;j<4;j++)
@@ -208,26 +208,26 @@ std::vector<double> probablityCalculator(std::vector<double> P)
         temp=e[i];
         e[i]=e[j];
         e[j]=temp;
-        
+
         temp=t[i];
         t[i]=t[j];
         t[j]=temp;
       }
     }
   }
-  
+
   e[0] = e[0]/3;
   e[1]= (1-e[0])*e[1]/3;
   e[2]=(1-e[0]-e[1])*e[2]/3;
   e[3]=(1-e[0]-e[1]-e[2]);
-  
+
   for(i=0;i<4;i++)
   {
     EmoProb[t[i]]=e[i];
-  }  
-  
+  }
+
   return EmoProb;
-}      
+}
 
 
 
@@ -268,4 +268,3 @@ int main(int argc,char** argv)
 	cin.ignore();
 	removePhotos();
 }
-
